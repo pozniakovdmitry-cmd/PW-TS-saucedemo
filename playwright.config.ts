234b167af27d0b43239fcabc7d9d4bfee62be72d
Dmitry Pozniakov <poznyakov.dmitry@gmail.com>
2026-04-26 14:38:07 +0100

    test2 : trigger Ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     baseURL: 'https://www.saucedemo.com',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
-    headless: false,
+    headless: true,
   },
 
 
